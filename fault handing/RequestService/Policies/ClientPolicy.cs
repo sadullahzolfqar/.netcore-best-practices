f31351a9d1da0cec5d757a7bd733b12de521b184
sadullahzolfqar <sadullah.zolfqar@gmail.com>
2022-02-22 18:23:07 +0300

    Add Polly fault handling retry

---
+++ fault handing/RequestService/Policies/ClientPolicy.cs
@@ -0,0 +1,22 @@
+﻿using Polly;
+using Polly.Retry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RequestService.Policies
+{
+    public class ClientPolicy
+    {
+        public AsyncRetryPolicy<HttpResponseMessage> ImmediateHttpRetry { get; }
+
+        public ClientPolicy()
+        {
+            ImmediateHttpRetry = Policy.HandleResult<HttpResponseMessage>(
+                res => !res.IsSuccessStatusCode
+                ).RetryAsync(10);
+        }
+    }
+}
